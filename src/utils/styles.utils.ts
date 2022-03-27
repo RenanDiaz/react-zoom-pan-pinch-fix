5d5bab9a59680aac454ee9b79ec50281d51d333f
nick <nick@byniko.com>
2022-03-27 14:32:58 -0700

    math round mitigate blurry?

--- src/utils/styles.utils.ts
+++ src/utils/styles.utils.ts
@@ -5,7 +5,7 @@ export const getTransformStyles = (
   y: number,
   scale: number,
 ): string => {
-  return `matrix(${scale}, 0,0, ${scale}, ${x}, ${y})`;
+  return `matrix(${scale}, 0,0, ${scale}, ${Math.round(x)}, ${Math.round(y)})`;
 };
 
 export const getCenterPosition = (
